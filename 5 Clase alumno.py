43ff1b7f253582607feada86523f4aa68bd840aa
Mañanas <omarp78@gmail.com>
2024-03-08 13:32:00 +0100

    primera carga de ficheros

---
+++ 5 Clase alumno.py
@@ -0,0 +1,22 @@
+# Definir una clase estudiante, crear varios estudiantes y meterlos en una lista
+class Estudiante():
+
+    def __init__(self,vnom,vape):
+        self.nombre = vnom
+        self.apellido = vape
+
+
+estudiantes = []
+cuantos=input('Nº alumnos:')
+
+for i in range(int(cuantos)):
+    print('-----------Alumno---------- ',i+1)
+    nombre = input('Nombre: ')
+    apellido = input('Apellido: ')
+
+    estudiante = Estudiante(nombre,apellido)
+
+    estudiantes.append(estudiante)
+
+
+print(estudiantes)
